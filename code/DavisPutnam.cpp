d333e7c40c63a02abcf44bba40d9a895d502b81d
Varun Vijaykumar <varun.vijaykumar75@gmail.com>
2016-07-15 18:39:01 +0200

    removing unwanted check from code
    Signed-off-by: Varun Vijaykumar <varun.vijaykumar75@gmail.com>

--- code/DavisPutnam.cpp
+++ code/DavisPutnam.cpp
@@ -14,14 +14,14 @@ bool davisPutnam::findPureLiterals(std::vector<std::vector<int> > cnf, int &pure
 {
     bool returnCode = false;
     size_t size_CNF = cnf.size();
-    
+
     if (cnf.size() != 0)
     {
         for (int checkElement = 1; checkElement <= m_MaxnetCount; checkElement++)
         {
             int positiveElement = 0;
             int negetiveElement = 0;
-            
+
             for (int i = 0; i < cnf.size(); i++)
             {
                 if (std::find(cnf[i].begin(), cnf[i].end(), checkElement) != cnf[i].end())
@@ -32,26 +32,8 @@ bool davisPutnam::findPureLiterals(std::vector<std::vector<int> > cnf, int &pure
                 {
                     negetiveElement++;
                 }
-                else
-                {
-                    std::vector<int> clause = cnf[i];
-                    size_t lastElement = clause.size();
-                    
-                    if (clause.size() != 0)
-                    {
-                        // checking the end element as well
-                        if (clause[lastElement - 1] == checkElement)
-                        {
-                            positiveElement++;
-                        }
-                        else if (clause[lastElement] == (-checkElement))
-                        {
-                            negetiveElement++;
-                        }
-                    }
-                }
             }
-            
+
             if (((negetiveElement > 0) && (positiveElement == 0)) ||
                 ((positiveElement > 0) && (negetiveElement == 0)))
             {
@@ -79,7 +61,7 @@ std::vector<std::vector<int> > davisPutnam::setVariableToOne(std::vector<std::ve
     {
         variableSet[literal] = 0;
     }
-    
+
     std::vector<std::vector<int> > newcnf;
     for (int clause = 0 ; clause < cnf.size() ; clause++)
     {
@@ -112,7 +94,7 @@ std::vector<std::vector<int> > davisPutnam::setVariableToOne(std::vector<std::ve
             newcnf.push_back(cnf[clause]);
         }
     }
-    
+
     return newcnf;
 }
 
@@ -121,7 +103,7 @@ bool davisPutnam::isPureLiteral(std::vector<std::vector<int> > cnf, int literalt
     for (int clause = 0 ; clause < cnf.size() ; clause++)
     {
         std::vector<int> interestClause = cnf[clause];
-        
+
         if (std::find(interestClause.begin(), interestClause.end(), -literaltoSearch) != interestClause.end())
         {
             return false;
@@ -148,7 +130,7 @@ bool davisPutnam::findemptyClause(std::vector<std::vector<int> > cnf)
     for (int clause = 0 ; clause < cnf.size() ; clause++)
     {
         std::vector<int> interestClause = cnf[clause];
-        
+
         if (interestClause.size() == 1)
         {
             if (std::find(interestClause.begin(), interestClause.end(),0) != interestClause.end())
@@ -157,7 +139,7 @@ bool davisPutnam::findemptyClause(std::vector<std::vector<int> > cnf)
             }
         }
     }
-    
+
     return false;
 }
 
@@ -167,12 +149,12 @@ void davisPutnam::algorithm(std::vector<std::vector<int> > cnf)
     int unitClauseObtained = 0;
     while( findPureLiterals(cnf, pureLiteral) || findUnitClause(cnf,unitClauseObtained) )
     {
-        
+
         while(findPureLiterals(cnf, pureLiteral))
         {
             std::vector<std::vector<int> > newcnf;
             // std::cout << "pure literal processing";
-            
+
 #ifdef DEBUG_ENABLED
             std::cout << "Set pure Literal " << pureLiteral << " with zero" << std::endl;
 #endif
@@ -182,33 +164,33 @@ void davisPutnam::algorithm(std::vector<std::vector<int> > cnf)
             std::cout << "After performing pure literal processing for " << pureLiteral << std::endl;
             printCNF(cnf);
 #endif
-            
+
         }
-        
+
         while (findUnitClause(cnf,unitClauseObtained))
         {
             std::vector<std::vector<int> > newcnf;
 #ifdef DEBUG_ENABLED
             std::cout << "Setting unit clause found:" << unitClauseObtained << " to One" << std::endl;
 #endif
-            
+
             newcnf = setVariableToOne(cnf, unitClauseObtained);
             cnf = newcnf;
-            
+
 #ifdef DEBUG_ENABLED
             std::cout << " CNF obtained after unitClause parsing for "<< unitClauseObtained << std::endl;
             printCNF(cnf);
 #endif
         }
-        
+
         break;
     }
-    
+
     // termination conditions
 #ifdef DEBUG_ENABLED
     std::cout << " Entering Termination part of the algorithm" << std::endl;
 #endif
-    
+
     if (cnf.size() == 0)
     {
         std::cout << "Netlists Not Equivalent" << std::endl;
@@ -218,20 +200,20 @@ void davisPutnam::algorithm(std::vector<std::vector<int> > cnf)
         std::map<std::string,int> netMap = m_net1.map;
         std::vector<std::string> output_names = m_net1.outputs;
         std::vector<std::string> output_names_net2 = m_net2.outputs;
-        
+
         //for inputs only
         for (int inpuNet = 0; inpuNet < input_Names.size(); inpuNet++ )
         {
             std::cout << input_Names[inpuNet] << " --> " <<variableSet[netMap[input_Names[inpuNet]]] << std::endl;
         }
-        
+
         //for outputs only ..
         for (int outNet = 0; outNet < output_names.size(); outNet++ )
         {
             std::cout << "netlist one output " << " --> " << variableSet[netMap[input_Names[outNet]]] << std::endl;
             std::cout << "netlist two output " << " --> " << variableSet[(netMap[input_Names[outNet]] + m_net2.netCount)] << std::endl;
         }
-        
+
         exit(0);
     }
     else if (findemptyClause(cnf))
@@ -241,20 +223,20 @@ void davisPutnam::algorithm(std::vector<std::vector<int> > cnf)
     else
     {
         // BACKTRACKING ALGORITHM
-        
+
 #ifdef DEBUG_ENABLED
         std::cout << " Entering Backtracking part of the algorithm" << std::endl;
         std::cout << "The rightmost element selected is : " << cnf[cnf.size()-1][cnf[cnf.size()-1].size()-1] << std::endl;
 #endif
-        
+
         int firstBurstElement = cnf.back().back();
         std::vector<std::vector<int> > cnf0 = setVariableToOne(cnf, -firstBurstElement);
 #ifdef DEBUG_ENABLED
         std::cout << "Setting rightmost element " << firstBurstElement << " to Zero " << std::endl;
         printCNF(cnf0);
-#endif        
+#endif
         algorithm(cnf0);
-        
+
         std::vector<std::vector<int> > cnf1 = setVariableToOne(cnf, firstBurstElement);
 #ifdef DEBUG_ENABLED
         std::cout << "Failed with Setting rightmost element " << firstBurstElement << " to Zero, now setting to One " << std::endl;
